72dcd54f52eb50b16c7fe4a5a9c223232e7361a7
Bailey <bailey.gage.davis@gmail.com>
2021-10-01 06:24:21 +0900

    lotta work

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
+from werkzeug.utils import redirect
 from surveys import Question, Survey, satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -18,4 +19,33 @@ def home_page():
 def form(id):
     """Shows survey page."""
 
-    return render_template("form.html", id=id)
+    #Prevents wrong question id from being manually entered in the address bar.
+    if id > len(responses):
+        flash('Invalid Question!')
+        return redirect(f"/questions/{len(responses)}")
+
+    return render_template("form.html", question=satisfaction_survey.questions[id])
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    """Post request to next survey question"""
+
+    choice = request.form["answer"]
+    responses.append(choice)
+
+    next_question_id = len(responses)
+
+    #Redirect to completion page when questions run out.
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect("/complete")
+
+    return redirect(f"/questions/{next_question_id}")   #Cannot format url like @app.route variable format
+
+@app.route("/complete")
+def complete():
+    """Shows the end of survey page."""
+
+    # Empty the response list to start from a clean slate.
+    responses.clear()
+
+    return render_template("complete.html")
