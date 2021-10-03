ceaf73009991f7808f579de88ed1036ab0924b51
Bailey <bailey.gage.davis@gmail.com>
2021-10-03 13:40:54 +0900

    added session storage for choices

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template, flash, session
+from flask.json import jsonify
 from werkzeug.utils import redirect
 from surveys import Question, Survey, satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
@@ -7,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 debug = DebugToolbarExtension(app)
 
-responses = []
+# responses = []
 
 @app.route("/")
 def home_page():
@@ -20,9 +21,9 @@ def form(id):
     """Shows survey page."""
 
     #Prevents wrong question id from being manually entered in the address bar.
-    if id > len(responses):
+    if id > len(session['responses']):
         flash('Invalid Question!')
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/questions/{len(session['responses'])}")
 
     return render_template("form.html", question=satisfaction_survey.questions[id])
 
@@ -31,12 +32,16 @@ def answer():
     """Post request to next survey question"""
 
     choice = request.form["answer"]
-    responses.append(choice)
 
-    next_question_id = len(responses)
+    #Append the choice to responses, but in a weird way for session to work, instead of just session['responses'].append(choice)
+    responses = session['responses']
+    responses.append(choice)
+    session['responses'] = responses
+
+    next_question_id = len(session['responses'])
 
     #Redirect to completion page when questions run out.
-    if len(responses) == len(satisfaction_survey.questions):
+    if len(session['responses']) == len(satisfaction_survey.questions):
         return redirect("/complete")
 
     return redirect(f"/questions/{next_question_id}")   #Cannot format url like @app.route variable format
@@ -45,7 +50,11 @@ def answer():
 def complete():
     """Shows the end of survey page."""
 
-    # Empty the response list to start from a clean slate.
-    responses.clear()
-
     return render_template("complete.html")
+
+@app.route("/session", methods=["POST"])
+def session_store():
+    # Empty the response list to start from a clean slate.
+    session['responses'] = []
+
+    return redirect("/questions/0")
